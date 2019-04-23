c9ae39e5817357978dd4d4a32c289c73e06e2a52
Luis Ruiz Pavón <luis_ruiz_pavon@hotmail.com>
2019-04-23 11:16:49 +0200

    Move feature store initilaization to fixture

---
+++ tests/FunctionalTests/Esquio.Configuration.Store/Fixture.cs
@@ -0,0 +1,31 @@
+﻿using Esquio.Abstractions;
+using Esquio.Configuration.Store;
+using Esquio.Configuration.Store.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.IO;
+
+namespace FunctionalTests.Esquio.Configuration.Store
+{
+    public class Fixture
+    {
+        public IFeatureStore FeatureStore { get; }
+
+        public Fixture()
+        {
+            var configuration = new ConfigurationBuilder()
+               .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "Esquio.Configuration.Store"))
+               .AddJsonFile("appsettings.json")
+               .Build();
+
+            var esquio = new EsquioConfiguration();
+            configuration.Bind("Esquio", esquio);
+
+            var options = Options.Create(esquio);
+            var logger = new LoggerFactory().CreateLogger<ConfigurationFeatureStore>();
+
+            FeatureStore = new ConfigurationFeatureStore(options, logger);
+        }
+    }
+}
