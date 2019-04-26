d7bb9ddf5893aeb534392d5378d71b9c91f9d2cb
Luis Ruiz Pavón <luis_ruiz_pavon@hotmail.com>
2019-04-26 14:31:49 +0200

    Add custom toggle sample

---
+++ samples/WebAppCustomToggle/Toggles/MyCustomToggle.cs
@@ -0,0 +1,23 @@
+﻿using Esquio.Abstractions;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace WebAppCustomToggle.Toggles
+{
+    public class MyCustomToggle : IToggle
+    {
+        private readonly ILogger<MyCustomToggle> _logger;
+
+        public MyCustomToggle(ILogger<MyCustomToggle> logger)
+        {
+            _logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
+        }
+
+        public Task<bool> IsActiveAsync(string featureName, string applicationName = null)
+        {
+            _logger.LogInformation($"Feature {featureName} for Application {applicationName} IsActiveAsync()");
+
+            return Task.FromResult(true);
+        }
+    }
+}
