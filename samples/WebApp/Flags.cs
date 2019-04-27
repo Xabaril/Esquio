cc83326d84662fb1809fda229314b22c108a351d
Quique Fdez Guerra <quique@ckgrafico.com>
2019-04-27 19:19:28 +0200

    Add new features for beta and alpha

--- samples/WebApp/Flags.cs
+++ samples/WebApp/Flags.cs
@@ -5,5 +5,7 @@
         public const string Privacy = nameof(Privacy);
         public const string MatchScore = nameof(MatchScore);
         public const string ByUser = nameof(ByUser);
+        public const string BetaUser = nameof(BetaUser);
+        public const string AlphaUser = nameof(AlphaUser);
     }
 }
---
+++ samples/WebApp/wwwroot/js/match/minutes.js
@@ -0,0 +1,5 @@
+﻿(function() {
+    // This JavaScript code helps to simulates how the page is reloading each minute (now 10s) and show new info
+    // In a real match we will receive the new info from server
+    alert('loaded');
+})();
