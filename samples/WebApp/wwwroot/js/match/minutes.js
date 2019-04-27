593d974ea7436a1d533f9a62c741b45ff71e2ef5
Quique Fdez Guerra <quique@ckgrafico.com>
2019-04-27 20:38:34 +0200

    Add minutes simulation

--- samples/WebApp/wwwroot/js/match/minutes.js
+++ samples/WebApp/wwwroot/js/match/minutes.js
@@ -8,7 +8,38 @@
                 return;
             }
 
-            alert('in progress');
+            // The idea of this code is to simulate that each minute (10s) the page reloads and retrieves more info from server
+            var POSITION_KEY = 'last_position';
+            var TIME = 10 * 1000;
+            var MAX = 60;
+            var HIDDEN_CLASS = 'is-hidden';
+            var lastPosition = sessionStorage[POSITION_KEY];
+
+            if (!lastPosition) {
+                lastPosition = '0';
+            }
+
+            lastPosition = Number(lastPosition);
+            [].reverse.call($('.js-minute')).each(function (i, minute) {
+                if (i > lastPosition) {
+                    return;
+                }
+
+                $(minute).removeClass(HIDDEN_CLASS);
+            });
+
+            $('.js-minutes').removeClass(HIDDEN_CLASS);
+            $('.js-minutes-progress').css('width', lastPosition + '%');
+
+            // We can reset when the number is too big for demo
+            if (lastPosition > MAX) {
+                lastPosition = -1;
+            }
+
+            setTimeout(() => {
+                sessionStorage[POSITION_KEY] = lastPosition + 1;
+                location.reload();
+            }, TIME);
         })(jQuery);
     });
 })();
