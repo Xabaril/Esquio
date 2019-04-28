91304bf010530cb6dfb6cf626c64ef7a3f093d0d
Quique Fdez Guerra <quique@ckgrafico.com>
2019-04-28 12:34:26 +0200

    Use render scripts section

--- samples/WebApp/wwwroot/js/match/live-minutes.js
+++ samples/WebApp/wwwroot/js/match/live-minutes.js
@@ -1,45 +1,41 @@
-﻿(function () {
+﻿(function ($) {
     // This JavaScript code helps to simulates how the data is live loading each minute (now 10s)
     // In a real match we will receive the new info from server
 
-    document.querySelector('body').addEventListener('scriptsLoaded', function () {
-        (function ($) {
-            // Use requestAnimationFrame if you care about performance (not in this demo :D)
-            var TIME = 10 * 1000;
-            var POSITION_KEY = 'last-position';
-            var MAX = 60;
-            var HIDDEN_CLASS = 'is-hidden';
+    var TIME = 10 * 1000;
+    var POSITION_KEY = 'last-position';
+    var MAX = 60;
+    var HIDDEN_CLASS = 'is-hidden';
 
-            function loadInfo() {
-                // The idea of this code is to simulate that each minute (10s) the loads more info from server
-                var lastPosition = sessionStorage[POSITION_KEY];
+    function loadInfo() {
+        // The idea of this code is to simulate that each minute (10s) the loads more info from server
+        var lastPosition = sessionStorage[POSITION_KEY];
 
-                if (!lastPosition) {
-                    lastPosition = '0';
-                }
+        if (!lastPosition) {
+            lastPosition = '0';
+        }
 
-                lastPosition = Number(lastPosition);
-                [].reverse.call($('.js-live-minute')).each(function (i, minute) {
-                    if (i > lastPosition) {
-                        return;
-                    }
-
-                    $(minute).removeClass(HIDDEN_CLASS);
-                });
-
-                $('.js-live-minutes').removeClass(HIDDEN_CLASS);
-                $('.js-live-minutes-progress').css('width', lastPosition + '%');
-
-                // We can reset when the number is too big for demo
-                if (lastPosition > MAX) {
-                    lastPosition = -1;
-                }
-
-                sessionStorage[POSITION_KEY] = lastPosition + 1;
+        lastPosition = Number(lastPosition);
+        [].reverse.call($('.js-live-minute')).each(function (i, minute) {
+            if (i > lastPosition) {
+                return;
             }
 
-            setInterval(loadInfo, TIME);
-            loadInfo();
-        })(jQuery);
-    });
-})();
+            $(minute).removeClass(HIDDEN_CLASS);
+        });
+
+        $('.js-live-minutes').removeClass(HIDDEN_CLASS);
+        $('.js-live-minutes-progress').css('width', lastPosition + '%');
+
+        // We can reset when the number is too big for demo
+        if (lastPosition > MAX) {
+            lastPosition = -1;
+        }
+
+        sessionStorage[POSITION_KEY] = lastPosition + 1;
+    }
+
+    // Use requestAnimationFrame if you care about performance (not in this demo :D)
+    setInterval(loadInfo, TIME);
+    loadInfo();
+})(jQuery);
--- samples/WebApp/wwwroot/js/match/minutes.js
+++ samples/WebApp/wwwroot/js/match/minutes.js
@@ -1,41 +1,37 @@
-﻿(function() {
+﻿(function ($) {
     // This JavaScript code helps to simulates how the page is reloading each minute (now 10s) and show new info
     // In a real match we will receive the new info from server
 
-    document.querySelector('body').addEventListener('scriptsLoaded', function () {
-        (function ($) {
-            // The idea of this code is to simulate that each minute (10s) the page reloads and retrieves more info from server
-            var POSITION_KEY = 'last_position';
-            var TIME = 10 * 1000;
-            var MAX = 60;
-            var HIDDEN_CLASS = 'is-hidden';
-            var lastPosition = sessionStorage[POSITION_KEY];
+    // The idea of this code is to simulate that each minute (10s) the page reloads and retrieves more info from server
+    var POSITION_KEY = 'last_position';
+    var TIME = 10 * 1000;
+    var MAX = 60;
+    var HIDDEN_CLASS = 'is-hidden';
+    var lastPosition = sessionStorage[POSITION_KEY];
 
-            if (!lastPosition) {
-                lastPosition = '0';
-            }
+    if (!lastPosition) {
+        lastPosition = '0';
+    }
 
-            lastPosition = Number(lastPosition);
-            [].reverse.call($('.js-minute')).each(function (i, minute) {
-                if (i > lastPosition) {
-                    return;
-                }
+    lastPosition = Number(lastPosition);
+    [].reverse.call($('.js-minute')).each(function (i, minute) {
+        if (i > lastPosition) {
+            return;
+        }
 
-                $(minute).removeClass(HIDDEN_CLASS);
-            });
-
-            $('.js-minutes').removeClass(HIDDEN_CLASS);
-            $('.js-minutes-progress').css('width', lastPosition + '%');
-
-            // We can reset when the number is too big for demo
-            if (lastPosition > MAX) {
-                lastPosition = -1;
-            }
-
-            setTimeout(function() {
-                sessionStorage[POSITION_KEY] = lastPosition + 1;
-                location.reload();
-            }, TIME);
-        })(jQuery);
+        $(minute).removeClass(HIDDEN_CLASS);
     });
-})();
+
+    $('.js-minutes').removeClass(HIDDEN_CLASS);
+    $('.js-minutes-progress').css('width', lastPosition + '%');
+
+    // We can reset when the number is too big for demo
+    if (lastPosition > MAX) {
+        lastPosition = -1;
+    }
+
+    setTimeout(function () {
+        sessionStorage[POSITION_KEY] = lastPosition + 1;
+        location.reload();
+    }, TIME);
+})(jQuery);
