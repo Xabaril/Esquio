a5461bb19cffbd6ca384c3eae24fe5af8cb6c002
Unai Zorrilla Castro <unai@plainconcepts.com>
2019-04-29 15:18:02 +0200

    ClaimValueToggle

---
+++ src/Esquio.AspNetCore/Toggles/ClaimValueToggle.cs
@@ -0,0 +1,54 @@
+﻿using Esquio.Abstractions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Esquio.AspNetCore.Toggles
+{
+    [DesignTypeParameter(ParameterName = ClaimType, ParameterType = "System.String", ParameterDescription = "The claim type used to check value.")]
+    [DesignTypeParameter(ParameterName = ClaimValues, ParameterType = "System.String", ParameterDescription = "The claim value to check, multiple items separated by ';'.")]
+    public class ClaimValueToggle
+        : IToggle
+    {
+        const string ClaimType = nameof(ClaimType);
+        const string ClaimValues = nameof(ClaimValues);
+        const char SPLIT_CHARACTER = ';';
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IFeatureStore _featureStore;
+        public ClaimValueToggle(IHttpContextAccessor httpContextAccessor, IFeatureStore store)
+        {
+            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+            _featureStore = store ?? throw new ArgumentNullException(nameof(store));
+        }
+        public async Task<bool> IsActiveAsync(string featureName, string applicationName = null)
+        {
+            var claimType = ((string)await _featureStore
+                .GetToggleParameterValueAsync<ClaimValueToggle>(featureName, applicationName, ClaimType));
+
+            var allowedValues = ((string)await _featureStore
+                .GetToggleParameterValueAsync<ClaimValueToggle>(featureName, applicationName, ClaimValues));
+
+            if (claimType != null
+                &&
+                ClaimValues != null)
+            {
+                var user = _httpContextAccessor.HttpContext.User;
+                if (user != null && user.Identity.IsAuthenticated)
+                {
+                    var value = user.FindFirst(claimType)?
+                        .Value;
+
+                    if (value != null)
+                    {
+                        return allowedValues
+                           .Split(SPLIT_CHARACTER)
+                           .Contains(value, StringComparer.InvariantCultureIgnoreCase);
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ tests/UnitTests/Esquio.AspNetCore/Toggles/ClaimValueToggleTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Esquio.AspNetCore.Toggles
+{
+    public class ClaimValueToggleShould
+    {
+    }
+}
