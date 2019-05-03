77facc5e61a4bf3ceed080aa4498ef68d74baa14
Unai Zorrilla Castro <unai@plainconcepts.com>
2019-05-03 12:11:33 +0200

    UserPreviewToggle added

---
+++ src/Esquio/Toggles/UserPreviewToggle.cs
@@ -0,0 +1,54 @@
+﻿using Esquio.Abstractions;
+using Esquio.Abstractions.Providers;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Esquio.Toggles
+{
+    [DesignTypeParameter(ParameterName = PreviewUsers, ParameterType = "System.String", ParameterDescription = "The collection of preview user(s) that can active this toggle separated  by ';' character")]
+    [DesignTypeParameter(ParameterName = EnabledUsers, ParameterType = "System.String", ParameterDescription = "The collection of preview enabled user(s) with this toggle active separated by ';' character")]
+    public class UserPreviewToggle
+         : IToggle
+    {
+        const string SPLIT_SEPARATOR = ";";
+        const string PreviewUsers = nameof(PreviewUsers);
+        const string EnabledUsers = nameof(EnabledUsers);
+
+        private readonly IUserNameProviderService _userNameProviderService;
+        private readonly IFeatureStore _featureStore;
+
+        public UserPreviewToggle(IUserNameProviderService userNameProviderService, IFeatureStore featureStore)
+        {
+            _userNameProviderService = userNameProviderService ?? throw new ArgumentNullException(nameof(userNameProviderService));
+            _featureStore = featureStore ?? throw new ArgumentNullException(nameof(featureStore));
+        }
+        public async Task<bool> IsActiveAsync(string featureName, string applicationName = null)
+        {
+            var currentUserName = await _userNameProviderService
+                .GetCurrentUserNameAsync();
+
+            if (currentUserName != null)
+            {
+                var previewUsers = (string)await _featureStore
+                    .GetToggleParameterValueAsync<UserNameToggle>(featureName, applicationName, PreviewUsers);
+
+                var enabledUsers = (string)await _featureStore
+                    .GetToggleParameterValueAsync<UserNameToggle>(featureName, applicationName, EnabledUsers);
+
+                if (previewUsers != null 
+                    &&
+                    previewUsers.Split(SPLIT_SEPARATOR).Contains(currentUserName, StringComparer.InvariantCultureIgnoreCase)
+                    &&
+                    enabledUsers != null
+                    &&
+                    enabledUsers.Split(SPLIT_SEPARATOR).Contains(currentUserName, StringComparer.InvariantCultureIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ tests/UnitTests/Esquio/Toggles/UserPreviewToggle.cs
@@ -0,0 +1,129 @@
+﻿using Esquio.Toggles;
+using FluentAssertions;
+using System;
+using System.Threading.Tasks;
+using UnitTests.Seedwork;
+using Xunit;
+
+namespace UnitTests.Esquio.Toggles
+{
+    public class UserPreviewToggleShould
+    {
+        [Fact]
+        public async Task be_not_active_if_user_provider_is_null()
+        {
+            var store = new DelegatedValueFeatureStore(_ => "user1;user2");
+
+            await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+            {
+                await new UserPreviewToggle(null, store).IsActiveAsync(Constants.FeatureName, Constants.ApplicationName);
+            });
+        }
+        [Fact]
+        public async Task be_not_active_if_user_is_null()
+        {
+            var store = new DelegatedValueFeatureStore(_ => "user2");
+            var userNameProvider = new DelegatedUserNameProviderService(() => null);
+
+            var active = await new UserPreviewToggle(userNameProvider, store).IsActiveAsync(Constants.FeatureName, Constants.ApplicationName);
+
+            active.Should().BeFalse();
+        }
+        [Fact]
+        public async Task be_not_active_if_preview_users_parameter_is_not_configured()
+        {
+            var store = new DelegatedValueFeatureStore(name =>
+            {
+                if (name == "EnabledUsers")
+                    return "User1";
+                else
+                    return null;
+            });
+
+            var userNameProvider = new DelegatedUserNameProviderService(() => "User1");
+
+            var active = await new UserPreviewToggle(userNameProvider, store).IsActiveAsync(Constants.FeatureName, Constants.ApplicationName);
+
+            active.Should().BeFalse();
+        }
+        [Fact]
+        public async Task be_not_active_if_enabled_users_parameter_is_not_configured()
+        {
+            var store = new DelegatedValueFeatureStore(name =>
+            {
+                if (name == "PreviewUsers")
+                    return "User1";
+                else
+                    return null;
+            });
+
+            var userNameProvider = new DelegatedUserNameProviderService(() => "User1");
+
+            var active = await new UserPreviewToggle(userNameProvider, store).IsActiveAsync(Constants.FeatureName, Constants.ApplicationName);
+
+            active.Should().BeFalse();
+        }
+        [Fact]
+        public async Task be_not_active_if_user_is_not_contained_on_preview_users_parameter_value()
+        {
+            var store = new DelegatedValueFeatureStore(name =>
+            {
+                if (name == "PreviewUsers")
+                    return "User3;User1";
+                else
+                    return "User3;User2";
+            });
+            var userNameProvider = new DelegatedUserNameProviderService(() => "User2");
+
+            var active = await new UserPreviewToggle(userNameProvider, store).IsActiveAsync(Constants.FeatureName, Constants.ApplicationName);
+
+            active.Should().BeFalse();
+        }
+        [Fact]
+        public async Task be_not_active_if_user_is_not_contained_on_enabled_users_parameter_value()
+        {
+            var store = new DelegatedValueFeatureStore(name =>
+            {
+                if (name == "PreviewUsers")
+                    return "User3;User2";
+                else
+                    return "User3;User1";
+            });
+            var userNameProvider = new DelegatedUserNameProviderService(() => "User2");
+
+            var active = await new UserPreviewToggle(userNameProvider, store).IsActiveAsync(Constants.FeatureName, Constants.ApplicationName);
+
+            active.Should().BeFalse();
+        }
+        [Fact]
+        public async Task be_active_if_user_is_on_preview_and_enabled_users_parameter_value()
+        {
+            var store = new DelegatedValueFeatureStore(name =>
+            {
+                if (name == "PreviewUsers")
+                    return "User3;User1";
+                else
+                    return "User3;User1";
+            });
+            var userNameProvider = new DelegatedUserNameProviderService(() => "user1");
+
+            var active = await new UserNameToggle(userNameProvider, store).IsActiveAsync(Constants.FeatureName, Constants.ApplicationName);
+            active.Should().BeTrue();
+        }
+        [Fact]
+        public async Task be_active_if_user_with_different_case_is_on_preview_and_enabled_users_parameter_value()
+        {
+            var store = new DelegatedValueFeatureStore(name =>
+            {
+                if (name == "PreviewUsers")
+                    return "User3;User1";
+                else
+                    return "User3;User1";
+            });
+            var userNameProvider = new DelegatedUserNameProviderService(() => "UsEr1");
+
+            var active = await new UserNameToggle(userNameProvider, store).IsActiveAsync(Constants.FeatureName, Constants.ApplicationName);
+            active.Should().BeTrue();
+        }
+    }
+}
