d31586256d6b6e88c06d32474e7389bfb4b03ed2
Quique Fdez Guerra <quique@ckgrafico.com>
2019-05-12 14:29:44 +0200

    Add dark theme

--- samples/WebApp/Flags.cs
+++ samples/WebApp/Flags.cs
@@ -6,5 +6,6 @@
         public const string MatchScore = nameof(MatchScore);
         public const string MinutesProgressBar = nameof(MinutesProgressBar);
         public const string MinutesRealTime = nameof(MinutesRealTime);
+        public const string DarkMode = nameof(DarkMode);
     }
 }
