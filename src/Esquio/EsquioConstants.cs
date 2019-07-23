240b67860e144707648e31a16a30d08f288a2c79
Unai Zorrilla Castro <unai@plainconcepts.com>
2019-07-23 23:47:16 +0200

    Added initial types

--- src/Esquio/EsquioConstants.cs
+++ src/Esquio/EsquioConstants.cs
@@ -5,6 +5,11 @@
         public static char[] DEFAULT_SPLIT_SEPARATOR = new char[] { ';' };
 
         public const string DEFAULT_PRODUCT_NAME = "default";
+
+        public const string SEMICOLON_LIST_PARAMETER_TYPE = "SemicolonList";
+        public const string PERCENTAGE_PARAMETER_TYPE = "Percentage";
+        public const string DATE_PARAMETER_TYPE = "Date";
+        public const string STRING_PARAMETER_TYPE = "String";
     }
 }
 
