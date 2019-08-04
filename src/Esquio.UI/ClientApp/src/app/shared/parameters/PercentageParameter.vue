a1327367aa9cfe8408c8be97344050b7693e59e6
Quique Fdez Guerra <quique@ckgrafico.com>
2019-08-04 15:19:15 +0200

    Add percentage

--- src/Esquio.UI/ClientApp/src/app/shared/parameters/PercentageParameter.vue
+++ src/Esquio.UI/ClientApp/src/app/shared/parameters/PercentageParameter.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="percentage_parameter">
-    <input
-      type="text"
-      v-model="value"
-    >
-
+    <div class="percentage_parameter-container">
+      <div class="percentage_parameter-slider">
+        <vue-slider v-model="value" :min="0" :max="100" :tooltip-formatter="formatter"></vue-slider>
+      </div>
+      <span class="percentage_parameter-value">{{value}}%</span>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
+import VueSlider from 'vue-slider-component';
 
-@Component
+@Component({
+  components: {
+    VueSlider
+  }
+})
 export default class extends Vue {
   public name = 'PercentageParameter';
-  public value = null;
+  public value = 0;
+  private isEmitting = false;
+  private t: number = null;
 
   @Prop({ required: true }) options: any;
 
@@ -22,21 +30,47 @@ export default class extends Vue {
     this.value = Number(this.options.value);
   }
 
+  public formatter(val) {
+    return val + '%';
+  }
+
   @Watch('value')
   onChangeValue(nextValue, prevValue) {
-    const nextValueNumber = Number(nextValue);
-    const prevValueNumber = Number(prevValue);
-
-    if (nextValueNumber > 100 || nextValueNumber < 0) {
-      this.value = prevValueNumber;
-      return;
+    if (this.isEmitting) {
+      clearTimeout(this.t);
     }
 
-    if (nextValueNumber === prevValueNumber) {
-      return;
-    }
+    this.isEmitting = true;
 
-    this.$emit('change', this.value);
+    this.t = setTimeout(() => {
+      this.$emit('change', this.value);
+      this.isEmitting = false;
+    }, 500);
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.percentage_parameter {
+  &-container {
+    display: block;
+    margin: 0 auto;
+    width: 80%;
+  }
+
+  &-slider {
+    display: inline-block;
+    width: 80%;
+  }
+
+  &-value {
+    color: get-color(secondary);
+    display: inline-block;
+    font-size: get-font-size(l);
+    font-weight: get-font-weight(bold);
+    margin-left: 5%;
+    transform: translateY(-.1rem);
+    width: 15%;
+  }
+}
+</style>
