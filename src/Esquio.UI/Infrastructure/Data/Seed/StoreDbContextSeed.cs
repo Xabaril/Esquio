6b5102eb4444e32e87ca536b5a405a104acf68b8
Unai Zorrilla Castro <unai@plainconcepts.com>
2019-09-27 16:02:32 +0200

    Added SubjectId default configuration

--- src/Esquio.UI/Infrastructure/Data/Seed/StoreDbContextSeed.cs
+++ src/Esquio.UI/Infrastructure/Data/Seed/StoreDbContextSeed.cs
@@ -1,5 +1,7 @@
 ﻿using Esquio.EntityFrameworkCore.Store;
 using Esquio.EntityFrameworkCore.Store.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
 
@@ -7,29 +9,25 @@ namespace Esquio.UI.Infrastructure.Data.Seed
 {
     public static class StoreDbContextSeed
     {
-        public static Action<StoreDbContext,IServiceProvider> Seed()
+        public static Action<StoreDbContext, IServiceProvider> Seed()
         {
+            const string DEFAULT_SUBJECT_ID_CONFIGURATION_KEY = "Security:DefaultSubjectId";
+
             return (context, sp) =>
             {
+                var configuration = sp.GetRequiredService<IConfiguration>();
+
                 if (!context.Permissions.Any())
                 {
                     var aliceIdSvrPermission = new PermissionEntity()
                     {
-                        SubjectId = "1",
+                        SubjectId = configuration[DEFAULT_SUBJECT_ID_CONFIGURATION_KEY] ?? "1",
                         ReadPermission = true,
                         WritePermission = true,
                         ManagementPermission = true,
                     };
 
-                    var unaiGoogleAccount = new PermissionEntity()
-                    {
-                        SubjectId = "65ad4d0aaa2941d03cce9cc9f595f55a6b30eddb068d8bf6b61fbd2599045953",
-                        ReadPermission = true,
-                        WritePermission = true,
-                        ManagementPermission = true,
-                    };
-                    
-                    context.Permissions.AddRange(aliceIdSvrPermission, unaiGoogleAccount);
+                    context.Permissions.AddRange(aliceIdSvrPermission);
 
                     context.SaveChanges();
                 }
