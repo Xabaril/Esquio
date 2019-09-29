55ac621ab8f9fade6c4cd2776d1be5d8cebd4310
Unai Zorrilla Castro <unai@plainconcepts.com>
2019-09-29 12:21:05 +0200

    Added functiona test to new users controllers

---
+++ tests/FunctionalTests/Esquio.UI.Api/Scenarios/Users/UsersControllerTests.cs
@@ -0,0 +1,218 @@
+﻿using Esquio.UI.Api.Features.Users.Add;
+using Esquio.UI.Api.Features.Users.My;
+using Esquio.UI.Api.Features.Users.Update;
+using FluentAssertions;
+using FunctionalTests.Esquio.UI.Api.Seedwork;
+using FunctionalTests.Esquio.UI.Api.Seedwork.Builders;
+using FunctionalTests.Esquio.UI.Api.Seedwork.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FunctionalTests.Esquio.UI.Api.Scenarios.Users
+{
+    [Collection(nameof(AspNetCoreServer))]
+    public class users_controller_should
+    {
+        private readonly ServerFixture _fixture;
+        public users_controller_should(ServerFixture serverFixture)
+        {
+            _fixture = serverFixture ?? throw new ArgumentNullException(nameof(serverFixture));
+        }
+
+        [Fact]
+        public async Task my_response_unauthorized_when_user_request_is_not_authenticated()
+        {
+            var response = await _fixture.TestServer
+                 .CreateRequest(ApiDefinitions.V1.Users.My())
+                 .GetAsync();
+
+            response.StatusCode
+                .Should()
+                .Be(StatusCodes.Status401Unauthorized);
+        }
+
+        [Fact]
+        [ResetDatabase]
+        public async Task my_response_forbidden_when_user_request_is_authenticated_but_user_does_not_have_permissions()
+        {
+            var response = await _fixture.TestServer
+                 .CreateRequest(ApiDefinitions.V1.Users.My())
+                 .WithIdentity(Builders.Identity().WithDefaultClaims().Build())
+                 .GetAsync();
+
+            response.StatusCode
+                .Should()
+                .Be(StatusCodes.Status403Forbidden);
+        }
+
+        [Fact]
+        [ResetDatabase]
+        public async Task my_response_permissions_when_authorized_user_request()
+        {
+            var permission = Builders.Permission()
+               .WithAllPrivilegesForDefaultIdentity()
+               .Build();
+
+            await _fixture.Given
+                .AddPermission(permission);
+
+
+            var response = await _fixture.TestServer
+                 .CreateRequest(ApiDefinitions.V1.Users.My())
+                 .WithIdentity(Builders.Identity().WithDefaultClaims().Build())
+                 .GetAsync();
+
+            response.StatusCode
+                .Should()
+                .Be(StatusCodes.Status200OK);
+
+            var content = await response.Content
+                 .ReadAs<MyResponse>();
+
+            content.ReadPermission
+                .Should().BeTrue();
+
+            content.WritePermission
+                .Should().BeTrue();
+
+            content.ManagementPermission
+                .Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task add_response_unauthorized_if_user_is_not_authenticated()
+        {
+            var response = await _fixture.TestServer
+                 .CreateRequest(ApiDefinitions.V1.Users.Add())
+                 .PostAsJsonAsync(new AddPermissionRequest());
+
+            response.StatusCode
+                .Should()
+                .Be(StatusCodes.Status401Unauthorized);
+        }
+
+        [Fact]
+        [ResetDatabase]
+        public async Task add_response_forbidden_if_user_is_not_authorized()
+        {
+            var permission = Builders.Permission()
+              .WithAllPrivilegesForDefaultIdentity()
+              .WithManagementPermission(false)
+              .Build();
+
+            await _fixture.Given
+                .AddPermission(permission);
+
+            var response = await _fixture.TestServer
+                 .CreateRequest(ApiDefinitions.V1.Users.Add())
+                 .WithIdentity(Builders.Identity().WithDefaultClaims().Build())
+                 .PostAsJsonAsync(new AddPermissionRequest());
+
+            response.StatusCode
+                .Should()
+                .Be(StatusCodes.Status403Forbidden);
+        }
+
+        [Fact]
+        [ResetDatabase]
+        public async Task add_response_ok_when_success()
+        {
+            var permission = Builders.Permission()
+              .WithAllPrivilegesForDefaultIdentity()
+              .Build();
+
+            await _fixture.Given
+                .AddPermission(permission);
+
+            var response = await _fixture.TestServer
+                 .CreateRequest(ApiDefinitions.V1.Users.Add())
+                 .WithIdentity(Builders.Identity().WithDefaultClaims().Build())
+                 .PostAsJsonAsync(new AddPermissionRequest()
+                 {
+                     SubjectId = Guid.NewGuid().ToString(),
+                     Read = true,
+                     Manage = true,
+                     Write = true
+                 });
+
+            response.StatusCode
+                .Should()
+                .Be(StatusCodes.Status200OK);
+        }
+
+        [Fact]
+        [ResetDatabase]
+        public async Task update_response_forbidden_if_user_is_not_authorized()
+        {
+            var permission = Builders.Permission()
+              .WithAllPrivilegesForDefaultIdentity()
+              .WithManagementPermission(false)
+              .Build();
+
+            await _fixture.Given
+                .AddPermission(permission);
+
+            var response = await _fixture.TestServer
+                 .CreateRequest(ApiDefinitions.V1.Users.Add())
+                 .WithIdentity(Builders.Identity().WithDefaultClaims().Build())
+                 .PutAsJsonAsync(new UpdatePermissionRequest());
+
+            response.StatusCode
+                .Should()
+                .Be(StatusCodes.Status403Forbidden);
+        }
+
+        [Fact]
+        public async Task update_response_unauthorized_if_user_is_not_authenticated()
+        {
+            var response = await _fixture.TestServer
+                 .CreateRequest(ApiDefinitions.V1.Users.Add())
+                 .PostAsJsonAsync(new UpdatePermissionRequest());
+
+            response.StatusCode
+                .Should()
+                .Be(StatusCodes.Status401Unauthorized);
+        }
+
+        [Fact]
+        [ResetDatabase]
+        public async Task update_response_ok_when_success()
+        {
+            var requesterPermission = Builders.Permission()
+              .WithAllPrivilegesForDefaultIdentity()
+              .Build();
+
+            var existingPermissionSubjectId = Guid.NewGuid().ToString();
+
+            var existingPermission = Builders.Permission()
+              .WithNameIdentifier(existingPermissionSubjectId)
+              .WithReadPermission(true)
+              .WithWritePermission(false)
+              .WithManagementPermission(false)
+              .Build();
+
+            await _fixture.Given
+                .AddPermission(requesterPermission, existingPermission);
+
+            var response = await _fixture.TestServer
+                 .CreateRequest(ApiDefinitions.V1.Users.Add())
+                 .WithIdentity(Builders.Identity().WithDefaultClaims().Build())
+                 .PutAsJsonAsync(new AddPermissionRequest()
+                 {
+                     SubjectId = existingPermissionSubjectId,
+                     Read = true,
+                     Manage = true,
+                     Write = true
+                 });
+
+            response.StatusCode
+                .Should()
+                .Be(StatusCodes.Status200OK);
+        }
+
+    }
+}
