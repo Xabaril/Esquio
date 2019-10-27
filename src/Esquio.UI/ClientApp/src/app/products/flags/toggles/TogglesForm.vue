4a3b38aab16be209564718cd616ff7e4512e2fb9
Eva Crespo <ecrespo@plainconcepts.com>
2019-10-27 22:15:38 +0100

    linting review

--- src/Esquio.UI/ClientApp/src/app/products/flags/toggles/TogglesForm.vue
+++ src/Esquio.UI/ClientApp/src/app/products/flags/toggles/TogglesForm.vue
@@ -446,8 +446,7 @@ export default class extends Vue {
         box-shadow: none;
       }
 
-      // stylelint-disable selector-no-qualifying-type
-      input[type='radio'] {
+      input {
         margin-left: 0;
         margin-top: 0;
       }
