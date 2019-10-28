4f6ede83d28088bcb23f3abd84820932b34d87d8
Unai Zorrilla Castro <unai@plainconcepts.com>
2019-10-28 21:43:35 +0100

    Added constants missing file

--- tools/Esquio.CliTool/Internal/Constants.cs
+++ tools/Esquio.CliTool/Internal/Constants.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace Esquio.CliTool.Internal
 {
-    class Constants
+    internal class Constants
     {
+        public const string ApiKeyParameter = "--api-key";
+        public const string ApiKeyDescription = "The valid Esquio UI Api Key used for Esquio authentication.";
+        public const string UriParameter = "--uri";
+        public const string UriDescription = "The Esquio UI url base path.";
     }
 }
