83540d4a34f23d028e1999e117473974be9251b5
Unai Zorrilla Castro <unai@plainconcepts.com>
2020-02-27 18:44:18 +0100

    created blazor client and host skeleton projects

---
+++ src/Esquio.UI.Host/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+using System.Net.Mime;
+
+namespace Esquio.UI.Host
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddResponseCompression(options =>
+            {
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { MediaTypeNames.Application.Octet });
+            });
+
+            services.AddMvc();
+            
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseResponseCompression();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBlazorDebugging();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+
+            app.UseClientSideBlazorFiles<Esquio.UI.Client.Program>();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapFallbackToClientSideBlazor<Esquio.UI.Client.Program>("index.html");
+            });
+        }
+    }
+}
