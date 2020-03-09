b68d3d59ee30064831e2d1664a6bd01087110b00
Unai Zorrilla Castro <unai@plainconcepts.com>
2020-03-09 20:20:56 +0100

    working on blazor ui authentication

--- src/Esquio.UI.Client/Program.cs
+++ src/Esquio.UI.Client/Program.cs
@@ -11,6 +11,8 @@ namespace Esquio.UI.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
+            //TODO: get from config!
+
             //add services
             builder.Services.AddBaseAddressHttpClient();
             builder.Services.AddOidcAuthentication(options =>
