0910dbcab4c3d8ce53fb9fc0e02160962b0d5dc0
Unai Zorrilla Castro <unai@plainconcepts.com>
2020-03-09 20:48:58 +0100

    Modified sdk package

--- src/Esquio.UI/ClientApp/src/app/tokens/TokensList.vue
+++ src/Esquio.UI/ClientApp/src/app/tokens/TokensList.vue
@@ -40,18 +40,18 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import { Inject } from "inversify-props";
-import { AlertType } from "~/core";
+import { Component, Vue } from 'vue-property-decorator';
+import { Inject } from 'inversify-props';
+import { AlertType } from '~/core';
 import {
   UserPermissions,
   PaginationInfo,
   IDateService,
   PaginatedTable
-} from "~/shared";
-import { ITokensService } from "./itokens.service";
-import { default as TokensForm } from "./TokensForm.vue";
-import { Token } from "./token.model";
+} from '~/shared';
+import { ITokensService } from './itokens.service';
+import { default as TokensForm } from './TokensForm.vue';
+import { Token } from './token.model';
 
 @Component({
   components: {
@@ -60,18 +60,18 @@ import { Token } from "./token.model";
   }
 })
 export default class extends Vue {
-  public name = "TokensList";
+  public name = 'TokensList';
   public tokens: Token[] = null;
   public isLoading = true;
   public paginationInfo = new PaginationInfo();
   public columns = [
     {
-      key: "name",
-      label: "tokens.fields.name"
+      key: 'name',
+      label: 'tokens.fields.name'
     },
     {
-      key: "extra",
-      label: "tokens.fields.validTo"
+      key: 'extra',
+      label: 'tokens.fields.validTo'
     }
   ];
 
@@ -104,23 +104,23 @@ export default class extends Vue {
       this.paginationInfo.rows = response.total;
       this.paginationInfo.pageIndex = response.pageIndex;
     } catch (e) {
-      this.$alert(this.$t("tokens.errors.get"), AlertType.Error);
+      this.$alert(this.$t('tokens.errors.get'), AlertType.Error);
     } finally {
       this.isLoading = false;
     }
   }
 
   private async deleteToken(token: Token): Promise<void> {
-    if (!(await this.$confirm(this.$t("tokens.confirm.title", [token.name])))) {
+    if (!(await this.$confirm(this.$t('tokens.confirm.title', [token.name])))) {
       return;
     }
 
     try {
       const response = await this.tokensService.remove(token);
       this.tokens = this.tokens.filter(x => x.name !== token.name);
-      this.$alert(this.$t("tokens.success.delete"));
+      this.$alert(this.$t('tokens.success.delete'));
     } catch (e) {
-      this.$alert(this.$t("tokens.errors.delete"), AlertType.Error);
+      this.$alert(this.$t('tokens.errors.delete'), AlertType.Error);
     } finally {
       this.isLoading = false;
     }
