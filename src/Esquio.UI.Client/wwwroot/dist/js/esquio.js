0bc7cfeeb545096d2ff44e38ca01eb53101bc274
Unai Zorrilla Castro <unai@plainconcepts.com>
2020-04-05 13:18:50 +0200

    Added copy to clipboard on new api key

--- src/Esquio.UI.Client/wwwroot/dist/js/esquio.js
+++ src/Esquio.UI.Client/wwwroot/dist/js/esquio.js
@@ -6,4 +6,36 @@
             $(identifier).jsonView(json, { collapsed: true });
         };
     }
+    if (!window.copyToClipboard) {
+        window.copyToClipboard = function (text) {
+            if (!navigator.clipboard) {
+                var textArea = document.createElement("textarea");
+                textArea.value = text;
+
+                // Avoid scrolling to bottom
+                textArea.style.top = "0";
+                textArea.style.left = "0";
+                textArea.style.position = "fixed";
+
+                document.body.appendChild(textArea);
+                textArea.focus();
+                textArea.select();
+
+                try {
+                    document.execCommand('copy');
+                } catch (err) {
+                    console.error('Fallback: Oops, unable to copy', err);
+                }
+
+                document.body.removeChild(textArea);
+            }
+            else {
+                navigator.clipboard.writeText(text).then(function () {
+                    console.log('Async: Copying to clipboard was successful!');
+                }, function (err) {
+                    console.error('Async: Could not copy text: ', err);
+                });
+            }
+        };
+    }
 })();
