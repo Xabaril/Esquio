0a21cb4ebca01793bb93553a7229f2d78e24859e
KYLE CRAVIOTTO <KYLE.CRAVIOTTO@LibertyMutual.com>
2020-07-20 11:43:00 -0700

    Add in ability to add multiple default users during seed

--- src/Esquio.UI.Host/Infrastructure/Data/Seed/StoreDbContextSeed.cs
+++ src/Esquio.UI.Host/Infrastructure/Data/Seed/StoreDbContextSeed.cs
@@ -3,6 +3,7 @@ using Esquio.UI.Api.Infrastructure.Data.Entities;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Esquio.UI.Host.Infrastructure.Data.Seed
@@ -12,7 +13,8 @@ namespace Esquio.UI.Host.Infrastructure.Data.Seed
         public static Action<StoreDbContext, IServiceProvider> Seed()
         {
             const string DEFAULT_SUBJECT_ID_CONFIGURATION_KEY = "Security:DefaultSubjectId";
-
+            const string DEFAULT_USERS = "Security:DefaultUsers";
+            
             return (context, sp) =>
             {
                 if (!context.Permissions.Any())
@@ -22,14 +24,21 @@ namespace Esquio.UI.Host.Infrastructure.Data.Seed
 
                     context.ApiKeys.Add(
                         new ApiKeyEntity("demo", DEFAULT_API_KEY, DateTime.UtcNow.AddYears(1)));
-
-                    var aliceIdSvrPermission = new PermissionEntity()
+                    var defaultUsers = new List<PermissionEntity>();
+                    foreach (var section in configuration.GetSection(DEFAULT_USERS)?.GetChildren()){
+                        var entity = new PermissionEntity{
+                            Kind = SubjectType.User
+                        };
+                        section.Bind(entity);
+                        defaultUsers.Add(entity);
+                    }
+                    var defaultIdSvrPermission = new PermissionEntity()
                     {
                         SubjectId = configuration[DEFAULT_SUBJECT_ID_CONFIGURATION_KEY] ?? "1", //default alice user
                         ApplicationRole = ApplicationRole.Management,
                         Kind = SubjectType.User
                     };
-
+                    defaultUsers.Add(defaultIdSvrPermission);
 
                     var apiKeyPermission = new PermissionEntity()
                     {
@@ -37,9 +46,9 @@ namespace Esquio.UI.Host.Infrastructure.Data.Seed
                         ApplicationRole = ApplicationRole.Reader,
                         Kind = SubjectType.Application
                     };
-
-                    context.Permissions.AddRange(aliceIdSvrPermission);
-
+                    defaultUsers.Add(apiKeyPermission);
+                    context.Permissions.AddRange(defaultUsers);
+                    
                     context.SaveChanges();
                 }
 
