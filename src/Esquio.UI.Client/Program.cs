ec5371eadf0b015f7497e4d2642f9e1afee5d496
Unai Zorrilla Castro <unai@plainconcepts.com>
2020-07-31 13:20:37 +0200

    Register msal or oidc script dynamically based on azure ad setting

--- src/Esquio.UI.Client/Program.cs
+++ src/Esquio.UI.Client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
 using System;
 using System.Threading.Tasks;
 
@@ -16,7 +17,67 @@ namespace Esquio.UI.Client
     {
         public static async Task Main(string[] args)
         {
-            await ConfigureHost(args).Build().RunAsync();
+            //await ConfigureHost(args).Build().RunAsync();
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.RootComponents
+                .Add<App>("app");
+
+            builder.Logging
+                .SetMinimumLevel(LogLevel.Warning);
+
+            builder.Services.AddHttpClient<IEsquioHttpClient, EsquioHttpClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            var isAzureAd = builder.Configuration.GetValue<bool>("Security:IsAzureAd");
+            var authority = builder.Configuration.GetValue<string>("Security:Authority");
+            var clientId = builder.Configuration.GetValue<string>("Security:ClientId");
+            var scope = builder.Configuration.GetValue<string>("Security:Scope");
+            var responseType = builder.Configuration.GetValue<string>("Security:ResponseType");
+
+            if (isAzureAd)
+            {
+                builder.Services.AddMsalAuthentication(options =>
+                {
+                    options.ProviderOptions.Authentication.Authority = authority;
+                    options.ProviderOptions.Authentication.ClientId = clientId;
+                    options.ProviderOptions.DefaultAccessTokenScopes.Add(scope);
+                });
+            }
+            else
+            {
+                builder.Services.AddOidcAuthentication(options =>
+                {
+                    options.ProviderOptions.Authority = authority;
+                    options.ProviderOptions.ClientId = clientId;
+                    options.ProviderOptions.ResponseType = responseType;
+                    options.ProviderOptions.DefaultScopes.Add(scope);
+                });
+            }
+
+           
+            builder.Services.AddAuthorizationCore(options =>
+            {
+                options.AddPolicy(Policies.Reader, builder => builder.AddRequirements(new PolicyRequirement(Policies.Reader)));
+                options.AddPolicy(Policies.Contributor, builder => builder.AddRequirements(new PolicyRequirement(Policies.Contributor)));
+                options.AddPolicy(Policies.Management, builder => builder.AddRequirements(new PolicyRequirement(Policies.Management)));
+            });
+
+            builder.Services.AddScoped<EsquioState>();
+            builder.Services.AddScoped<ConfirmState>();
+            builder.Services.AddScoped<ILocalStorage, LocalStorage>();
+            builder.Services.AddScoped<IPolicyBuilder, DefaultPolicyBuilder>();
+            builder.Services.AddScoped<INotifications, Notifications>();
+            builder.Services.AddScoped<IAuthorizationHandler, PolicyRequirementHandler>();
+
+
+            var host =  builder.Build();
+
+            var jsRuntime = builder.Services.BuildServiceProvider().GetRequiredService<IJSRuntime>();
+            await jsRuntime.InvokeVoidAsync("addAuthenticationScript", new object[] { isAzureAd });
+
+            await host.RunAsync();
+
         }
 
         static WebAssemblyHostBuilder ConfigureHost(string[] args)
@@ -32,9 +93,7 @@ namespace Esquio.UI.Client
             builder.Services.AddHttpClient<IEsquioHttpClient, EsquioHttpClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
-            var isAzureAd = builder.Configuration
-                .GetValue<bool>("Security:IsAzureAd");
-
+            var isAzureAd = builder.Configuration.GetValue<bool>("Security:IsAzureAd");
             var authority = builder.Configuration.GetValue<string>("Security:Authority");
             var clientId = builder.Configuration.GetValue<string>("Security:ClientId");
             var scope = builder.Configuration.GetValue<string>("Security:Scope");
@@ -45,10 +104,8 @@ namespace Esquio.UI.Client
                 builder.Services.AddMsalAuthentication(options =>
                 {
                     options.ProviderOptions.Authentication.Authority = authority;
-                    options.ProviderOptions.Authentication.ClientId = clientId; 
-                    //options.ProviderOptions.Authentication.RedirectUri = "https://localhost:44359/authentication/login-callback";
+                    options.ProviderOptions.Authentication.ClientId = clientId;
                     options.ProviderOptions.DefaultAccessTokenScopes.Add(scope);
-
                 });
             }
             else
@@ -61,6 +118,9 @@ namespace Esquio.UI.Client
                     options.ProviderOptions.DefaultScopes.Add(scope);
                 });
             }
+
+            var jsRuntime = builder.Services.BuildServiceProvider().GetRequiredService<IJSRuntime>();
+            jsRuntime.InvokeVoidAsync("addAuthenticationScript", new object[] { isAzureAd });
 
             builder.Services.AddAuthorizationCore(options =>
             {
@@ -75,6 +135,7 @@ namespace Esquio.UI.Client
             builder.Services.AddScoped<IPolicyBuilder, DefaultPolicyBuilder>();
             builder.Services.AddScoped<INotifications, Notifications>();
             builder.Services.AddScoped<IAuthorizationHandler, PolicyRequirementHandler>();
+
 
             return builder;
         }
--- src/Esquio.UI.Client/wwwroot/js/esquio.js
+++ src/Esquio.UI.Client/wwwroot/js/esquio.js
@@ -1,5 +1,19 @@
 ﻿(function () {
 
+
+    if (!window.addAuthenticationScript) {
+        window.addAuthenticationScript = function (isAzureAd) {
+            console.log("isAzureAd:" + isAzureAd);
+            if (isAzureAd)
+                console.log("que si que lo es");
+
+            var script = document.createElement("script");
+            script.type = "text/javascript";
+            script.src = isAzureAd ? "_content/Microsoft.Authentication.WebAssembly.Msal/AuthenticationService.js" : "_content/Microsoft.AspNetCore.Components.WebAssembly.Authentication/AuthenticationService.js";
+            $("head").append(script);
+        };
+    }
+
     if (!window.exportProduct) {
         window.exportProduct = function (json) {
             let a = document.createElement('a');
