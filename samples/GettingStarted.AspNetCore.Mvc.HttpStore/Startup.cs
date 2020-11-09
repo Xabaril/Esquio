25991cb8918f409a5cfc8f146212714a0808f3c0
Unai Zorrilla Castro <unai@plainconcepts.com>
2020-11-09 17:07:20 +0100

    Add polly retry sample on Http Store samples

--- samples/GettingStarted.AspNetCore.Mvc.HttpStore/Startup.cs
+++ samples/GettingStarted.AspNetCore.Mvc.HttpStore/Startup.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Polly;
+using Polly.Extensions.Http;
+using System;
+using System.Net.Http;
 
 namespace GettingStarted.AspNetCore.Mvc
 {
@@ -24,7 +28,8 @@ namespace GettingStarted.AspNetCore.Mvc
                 {
                     options.UseBaseAddress("http://localhost:1368/") //this is Esquio UI base address
                         .UseApiKey("b6+KYpSY8VPMBmHLNJ00z80aPOe+Li4EGe4idoKKI1A=") // this is a Api Key on Esquio UI (only Reader permission is Required) 
-                        .UseCache(false); // you can configure cache use and expiration timestamp, false is defualt value
+                        .UseCache(false)// you can configure cache use and expiration timestamp, false is defualt value
+                        .ConfigureHttpClient(http => http.AddPolicyHandler(GetRetryPolicy())); 
                 })
                 .Services
                 .AddControllersWithViews();
@@ -54,6 +59,13 @@ namespace GettingStarted.AspNetCore.Mvc
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
         }
     }
 }
