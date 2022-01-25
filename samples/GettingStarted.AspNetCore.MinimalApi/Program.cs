b4379d1595fb61169fa48b7e8a97706f88cb8317
unaizorrilla <unai@plainconcepts.com>
2022-01-25 17:18:27 +0100

    Added minimal api sample

---
+++ samples/GettingStarted.AspNetCore.MinimalApi/Program.cs
@@ -0,0 +1,45 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Esquio dependencies to the container
+builder.Services.AddEsquio()
+    .AddAspNetCoreDefaultServices()
+    .AddConfigurationStore(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+       new WeatherForecast
+       (
+           DateTime.Now.AddDays(index),
+           Random.Shared.Next(-20, 55),
+           summaries[Random.Shared.Next(summaries.Length)]
+       ))
+        .ToArray();
+    return forecast;
+})
+.RequireFeature("AwesomeFeature")
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
